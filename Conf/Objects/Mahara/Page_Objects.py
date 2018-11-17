91ed65f32c784579a07da05469d1d2b628e249af
Jose Rodriguez Rozo <44528731+unikolajky10@users.noreply.github.com>
2018-11-17 15:24:42 +0100

    Create Page_Objects.py

---
+++ Conf/Objects/Mahara/Page_Objects.py
@@ -0,0 +1,9 @@
+# Main Page
+input_username = "id=login_login_username"
+input_password = "name=login_password"
+btn_login = "Login"
+error_messages = "xpath=//div[@id='messages']"
+
+# Profile Page
+frame_profile = "xpath=//a[@title='Profile page']"
+profile_block = "xpath=//h3[@class='panel-heading profile-block']"
