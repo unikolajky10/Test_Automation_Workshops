bc1949693d2ef8405d36437e843e462b5d886b9a
Jose Rodriguez Rozo <44528731+unikolajky10@users.noreply.github.com>
2018-11-17 15:25:28 +0100

    Create Credentials.py

---
+++ Conf/Settings/Mahara/Credentials.py
@@ -0,0 +1,13 @@
+#Invalid User
+invalid_username = "invalidname"
+invalid_password = "invalid password"
+
+#Admin User
+admin_username = "admin"
+admin_password = "MaharaDemo"  
+admin_name = "James Jetts"
+ 
+#Student User
+student_user = "student"
+student_password = "MaharaDemo"
+student_name = "Paula Paulsen"
