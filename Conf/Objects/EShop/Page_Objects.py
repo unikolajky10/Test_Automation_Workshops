56fc2879539ddd881ef72e4632be1751acb30fb6
Jose Rodriguez Rozo <44528731+unikolajky10@users.noreply.github.com>
2018-11-18 15:42:52 +0100

    Create Page_Objects.py

---
+++ Conf/Objects/EShop/Page_Objects.py
@@ -0,0 +1,13 @@
+# Other
+
+Sort_dropdown = 'id=selectProductSort'
+List = 'id=list'
+
+Dresses_section = 'xpath=//*[@id="categories_block_left"]/div/ul/li[2]/a'
+Evening_dresses_subsection = 'xpath=//*[@id="categories_block_left"]/div/ul/li[2]/a'
+Category_name = 'class=cat-name'
+
+Center_Column = 'xpath=//*[@id="center_column"]/ul/p'
+
+Filter_categories_dresses_checkbox = 'id=layered_category_8'
+Enabled_filters = 'id=enabled_filters'
